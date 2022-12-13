7955837b63f9ab0d2b1beb2260d2b1e48ef98478
Emilio Cruciani <emilio.cruciani@gmail.com>
2022-12-13 10:50:06 +0100

    Improve usability of script (add default values); update README

--- brain-alignment.py
+++ brain-alignment.py
@@ -1,10 +1,11 @@
 import argparse
-
 import os
 import pickle
 import sys
+
 import numpy as np
 from scipy.optimize import linear_sum_assignment
+
 
 
 def getSpatialInfo(animal, n):
@@ -12,6 +13,7 @@ def getSpatialInfo(animal, n):
     M -= np.asarray([[np.mean(M[:,0]), np.mean(M[:,1]), np.mean(M[:,2])] for _ in range(n)])  # centering
     M *= (0.75/np.pi)**(1/3) / np.linalg.norm(max(M, key=np.linalg.norm))  # normalization
     return M
+
 
 def getConnectivityInfo(animal):
     return np.loadtxt(os.path.join("brains-matrix", "{}.txt".format(animal)))
@@ -66,26 +68,8 @@ def computeAlignmentDist(Xs, Ys, Xc, Yc, n, a, b):
     return (alignment, distance)
 
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Brain Alignment")
-    parser.add_argument("-a1", "--animal1", type=str, metavar="", required=True, help="Input Animal1")
-    parser.add_argument("-a2", "--animal2", type=str, metavar="", required=True, help="Input Animal2")
-    parser.add_argument("-n", "--numberpoints", type=int, metavar="", required=True, help="Number of Points")
-    parser.add_argument("-d", "--degree", type=int, metavar="", required=True, help="Degree of Canonical Rotations")
-    parser.add_argument("-r", "--rotations", type=int, metavar="", required=True, help="Number of Random Rotations")
-    parser.add_argument("-a", "--coeffA", type=float, metavar="", required=True, help="Coefficient A (Euclidean)")
-    parser.add_argument("-b", "--coeffB", type=float, metavar="", required=True, help="Coefficient B (StrongestEdge)")
-    parser.add_argument("-s", "--saveAlignment", action="store_true", help="Save Alignment")
-    args = parser.parse_args()
-
-    # spatial info
-    (Xs, Xc) = (getSpatialInfo(args.animal1, args.numberpoints), getConnectivityInfo(args.animal1))
-    (Ys, Yc) = (getSpatialInfo(args.animal2, args.numberpoints), getConnectivityInfo(args.animal2))
-    
+def computeBestCanonicalRotation(Xs, Ys, Xc, Yc, args):
     (alignment, distance) = computeAlignmentDist(Xs, Ys, Xc, Yc, args.numberpoints, args.coeffA, args.coeffB)
-    
-    # find best canonical rotation
     (alpha_star, beta_star, gamma_star) = (0, 0, 0)
     for alpha in range(0, 360, args.degree):
         for beta in range(0, 360, args.degree):
@@ -95,11 +79,12 @@ if __name__ == "__main__":
                 if distance_new < distance:
                     distance = distance_new
                     (alpha_star, beta_star, gamma_star) = (alpha, beta, gamma)
-    # apply best canonical rotation
     Ys = rotateMatrix(Ys, alpha_star, beta_star, gamma_star)
-    (alignment, distance) = computeAlignmentDist(Xs, Ys, Xc, Yc, args.numberpoints, args.coeffA, args.coeffB)
+    return Ys
 
-    # find best random small rotation
+
+def computeBestSmallRotation(Xs, Ys, Xc, Yc, args):
+    (alignment, distance) = computeAlignmentDist(Xs, Ys, Xc, Yc, args.numberpoints, args.coeffA, args.coeffB)
     for _ in range(args.rotations):
         (alpha, beta, gamma) = (np.random.normal(0, 1), np.random.normal(0, 1), np.random.normal(0, 1))
         Ws = rotateMatrix(Ys, alpha, beta, gamma)
@@ -107,8 +92,37 @@ if __name__ == "__main__":
         if distance_new < distance:
             (alignment, distance) = (alignment_new, distance_new)
             Ys = Ws
+    return Ys
+
+
+def computeBrainAlignment(Xs, Ys, Xc, Yc, args):
+    Ys = computeBestCanonicalRotation(Xs, Ys, Xc, Yc, args)
+    Ys = computeBestSmallRotation(Xs, Ys, Xc, Yc, args)
     (alignment, distance) = computeAlignmentDist(Xs, Ys, Xc, Yc, args.numberpoints, args.coeffA, args.coeffB)
+    return (alignment, distance)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script for Brain Alignment")
+    parser.add_argument("-a1", "--animal1", type=str, metavar="", required=True, help="Input animal 1")
+    parser.add_argument("-a2", "--animal2", type=str, metavar="", required=True, help="Input animal 2")
+    parser.add_argument("-n", "--numberpoints", type=int, default=200, metavar="", help="Number of points for a brain")
+    parser.add_argument("-d", "--degree", type=int, default=90, metavar="", help="Degrees of canonical rotations")
+    parser.add_argument("-r", "--rotations", type=int, default=150, metavar="", help="Number of random rotations")
+    parser.add_argument("-a", "--coeffA", type=float, default=1, metavar="", help="Coefficient A (Euclidean) in the distance computation")
+    parser.add_argument("-b", "--coeffB", type=float, default=1, metavar="", help="Coefficient B (StrongestEdge) in the distance computation")
+    parser.add_argument("-s", "--saveAlignment", action="store_true", help="Save alignment to file")
+    args = parser.parse_args()
+
+    # load data
+    (Xs, Xc) = (getSpatialInfo(args.animal1, args.numberpoints), getConnectivityInfo(args.animal1))
+    (Ys, Yc) = (getSpatialInfo(args.animal2, args.numberpoints), getConnectivityInfo(args.animal2))
     
+    # compute alignment
+    (alignment, distance) = computeBrainAlignment(Xs, Ys, Xc, Yc, args)
+    
+    # write results to file
     os.makedirs("distance", exist_ok=True)
     with open(os.path.join("distance", f"{args.animal1}_vs_{args.animal2}.pickle"), "wb") as handle:
         pickle.dump(distance, handle)
